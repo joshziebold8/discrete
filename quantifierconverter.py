ae5ac39a44ed3a0d2892606ee8dfb64fd02d93f3
joshziebold8 <jozi1518@colorado.edu>
2021-09-10 22:24:09 +0000

    bruh

--- quantifierconverter.py
+++ quantifierconverter.py
@@ -4,7 +4,7 @@ class QuantifierConverter(object):
 
 	def quantifierConverter(self,s):
 		#Start writing your code from here
-            A = A + '\u2200'
-            E = E + '\u2203'
+            A = '\u2200'
+            E = '\u2203'
             str2 = s.replace("for every", A)
             return str2.replace("there exists", E)
