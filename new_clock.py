16b95064310a7037b1a2390cacbe9ac32e6c861a
joshziebold8 <jozi1518@colorado.edu>
2021-10-08 19:38:30 -0600

    Clock

--- new_clock.py
+++ new_clock.py
@@ -2,7 +2,7 @@ class Clock:
 
     def get_position(self, max_hours, mins_passed):
         hour_position = 0
-        for i in range(0, mins_passed/60):
+        for i in range(0, mins_passed//60):
             hour_position+=1
             if max_hours == hour_position: hour_position = 0
             i+=1
