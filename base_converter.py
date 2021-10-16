5747547791a053d8e72f022b9c9244902123d462
joshziebold8 <jozi1518@colorado.edu>
2021-10-15 19:34:28 -0600

    hello

---
+++ base_converter.py
@@ -0,0 +1,9 @@
+class BaseConverter:
+
+	def base_converter(self, num, from_base, to_base):
+		sum = 0
+		x = len(num)-1
+		for i in num: #to decimal
+			sum += pow(from_base, x) * ord(i)
+			x -= 1
+		return sum
