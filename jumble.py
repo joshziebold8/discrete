624a5c5beae4fd3da4481acf3ce9006156d2fd85
joshziebold8 <jozi1518@colorado.edu>
2021-11-05 19:43:53 -0600

    maybe?

--- jumble.py
+++ jumble.py
@@ -3,11 +3,11 @@ class Jumble():
 	def get_total_distinct_combinations(self, input_string):
 		import math
 		str = input_string.lower()
-		x = 0
+		x = -1
 		temp = ""
 		for i in str:
 			if str.count(i) == 1 or temp.count(i) == 0:
 				temp += i
 				x+=1
-		print(x, len(str))
+		##print(x, len(str))
 		return math.factorial(len(str)) / math.factorial(len(str)-x)
