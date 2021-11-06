b2a170cf3d321f5eea33381385622f142bc4e812
joshziebold8 <jozi1518@colorado.edu>
2021-11-05 20:07:06 -0600

    this will work

--- jumble.py
+++ jumble.py
@@ -2,11 +2,12 @@ class Jumble():
 	
 	def get_total_distinct_combinations(self, input_string):
 		import math
+		str = input_string.lower()
 		count = [0] * 26
-		for i in range(0, len(input_string)):
-			if (input_string[i] >= 'a'):
-				count[(ord)(input_string[i]) - 97] += 1
+		for i in range(0, len(str)):
+			if (str[i] >= 'a'):
+				count[(ord)(str[i]) - 97] += 1
 		factorial = 1
 		for i in range(0, 26) :
 			factorial *= math.factorial(count[i])
-		return math.factorial(len(input_string)) / factorial
+		return math.factorial(len(str)) / factorial
