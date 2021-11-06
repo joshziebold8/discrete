e8973a906b1d13d4ee3e5ce822fee4a446fd1c45
joshziebold8 <jozi1518@colorado.edu>
2021-11-05 19:56:53 -0600

    maybe?

--- jumble.py
+++ jumble.py
@@ -7,12 +7,12 @@ class Jumble():
 		y = 0
 		temp = ""
 		for i in str:
+			if str.count(i) > 1 and temp.count(i) == 0:
+				y += 1
 			if str.count(i) == 1 or temp.count(i) == 0:
 				temp += i
 				x+=1
-			if str.count(i) > 1:
-				y += 1
 		print(x, len(str))
 		if y == 0:
 			return math.factorial(len(str)) / math.factorial(len(str)-x)
-		return (math.factorial(len(str)) / math.factorial(len(str)-x))
+		return (math.factorial(len(str)) / math.factorial(len(str)-x)) / y
