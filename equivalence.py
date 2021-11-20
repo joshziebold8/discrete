71bd07fe930e2604a2aa14dcf0191afcc59cf673
joshziebold8 <jozi1518@colorado.edu>
2021-11-20 16:00:33 -0700

    first test

--- equivalence.py
+++ equivalence.py
@@ -1,23 +1,24 @@
 class EquivalenceRelation():
-
+	
 	def check_equivalence(self, relation_members):
 		reflexive = True
 		symmetric = True
 		transitive = True
-		for i in relation_members: #reflexive
-			if not relation_members(i, i):
+		set = {0, 1, 2, 3}
+		for i in set: #reflexive
+			if (i, i) not in relation_members:
 				reflexive = False
 
-		for i in relation_members: #symmetric
-			for j in relation_members:
-				if relation_members(i, j) != relation_members(i, j):
-					symmetric = False
+		for i, j in relation_members: #symmetric
+			if (j, i) not in relation_members:
+				symmetric = False
 
-		for i in relation_members:
-			for j in relation_members:
-				for x in relation_members:
-					if (relation_members(i, j) and relation_members(j, x)) and (not relation_members(i, x)):
-						transitive = False
+		for i in set:
+			for j in set:
+				if (i, j) in relation_members:
+					for x in set:
+						if (j, x) in relation_members and (i, x) not in relation_members:
+							transitive = False
 
 
 		if(reflexive and symmetric and transitive):
