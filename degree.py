fbd19a41a667f4cc89ca283ee4b23583e8170b31
joshziebold8 <jozi1518@colorado.edu>
2021-12-05 14:23:44 -0700

    first try

--- degree.py
+++ degree.py
@@ -4,11 +4,11 @@ class Graph():
 		''' A here is the adjacency matrix '''
 		out = [len(A)]
 		inD = [len(A)]
-		ans = ''
+		ans = []
 		for i in range(len(A)):
 			for j in range(len(A)):
 				if A[i][j] == 1:
 					out[i] += 1
 					inD[j] += 1
-			ans += "(" + out[i] + "," + inD[i] + "),"	
+			ans[i] = "(" + out[i] + "," + inD[i] + "),"	
 		return ans
