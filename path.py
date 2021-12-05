9cda78589c3cb9dbb0f8cd5047b417b792a36531
joshziebold8 <jozi1518@colorado.edu>
2021-12-05 14:33:32 -0700

    first try

--- path.py
+++ path.py
@@ -3,5 +3,4 @@ class Graph():
 	def is_path(self, A, start_node, end_node):
 		''' A here is the adjacency matrix 
 		return True if there is a path from start_node to end_node'''
-		
-		#Your code here
+		return True
